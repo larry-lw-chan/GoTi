6aed7cfbaee83eb088258c31796c4819fddb9757
Larry Chan <1851426+larry-lw-chan@users.noreply.github.com>
2024-02-14 13:34:10 -0500

    Reorganized routes into router package

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,26 +4,12 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-	"github.com/larry-lw-chan/go-tiny/packages/handlers"
+	"github.com/larry-lw-chan/go-tiny/packages/router"
 )
 
 func main() {
-	// Define Routes Here
-	r := chi.NewRouter()
-
-	// Middleware
-	r.Use(middleware.Logger)
-
-	// Static File Server
-	fs := http.FileServer(http.Dir("static"))
-	r.Handle("/static/*", http.StripPrefix("/static/", fs))
-
-	// Routes
-	r.Get("/", handlers.HomePage)
-	r.Get("/about", handlers.AboutPage)
-	r.Get("/contact", handlers.ContactPage)
+	// Get Routes
+	r := router.Routes()
 
 	// Start Server
 	fmt.Println("Server is running at :3000")
---
+++ packages/router/routes.go
@@ -0,0 +1,38 @@
+package router
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/larry-lw-chan/go-tiny/packages/handlers"
+)
+
+func Routes() *chi.Mux {
+	// Define Routes Here
+	r := chi.NewRouter()
+
+	// A good base middleware stack (Chi Default)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	// Set a timeout value on the request context (ctx), that will signal
+	// through ctx.Done() that the request has timed out and further
+	// processing should be stopped.
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	// Static File Server
+	fs := http.FileServer(http.Dir("static"))
+	r.Handle("/static/*", http.StripPrefix("/static/", fs))
+
+	// Routes
+	r.Get("/", handlers.HomePage)
+	r.Get("/about", handlers.AboutPage)
+	r.Get("/contact", handlers.ContactPage)
+
+	// Return the Router
+	return r
+}
