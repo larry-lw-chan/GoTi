dd2a2b99c8d1d9059fd4002e9ba5e7cfd2da9f3a
Larry Chan <1851426+larry-lw-chan@users.noreply.github.com>
2024-02-14 09:09:12 -0500

    Got template to recognize partials now

--- utils/render.go
+++ utils/render.go
@@ -9,9 +9,12 @@ import (
 var tmplPath = "./templates"
 var tmplLayout = "./templates/layouts/base.layout.tmpl"
 
+var tmplNav = "./templates/partials/nav.partial.tmpl"
+
 func Render(w http.ResponseWriter, tmpl string, data interface{}) {
 	tmpl = tmplPath + tmpl
-	t := template.Must(template.ParseFiles(tmpl, tmplLayout))
+	t := template.Must(template.ParseFiles(tmpl, tmplNav, tmplLayout))
+
 	err := t.Execute(w, data)
 	if err != nil {
 		log.Println(err)
