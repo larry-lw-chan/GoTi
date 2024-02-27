7d84ec928668fa744ec93543dd89f34cfa96b870
Larry Chan <1851426+larry-lw-chan@users.noreply.github.com>
2024-02-27 12:07:49 -0500

    Got animated hamburger icon

---
+++ static/scripts/global.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+  // Hamburger Menu Toggle
+  let hamburger = document.getElementById("hamburger");
+  hamburger.addEventListener("click", function () {
+    this.classList.toggle("change");
+  });
+};
