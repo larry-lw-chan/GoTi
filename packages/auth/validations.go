dd1f45ca95bd045e8079710ce47dac2078aa5f2b
Larry Chan <1851426+larry-lw-chan@users.noreply.github.com>
2024-03-11 17:15:48 -0400

    Got homepage clientside validation and server side validation working

--- packages/auth/validations.go
+++ packages/auth/validations.go
@@ -13,7 +13,7 @@ var (
 	validate *validator.Validate
 )
 
-type UserValidation struct {
+type CreateUserValidation struct {
 	Username string `validate:"required,min=4,max=15"`
 	Email    string `validate:"required,email"`
 	Password string `validate:"required,min=8"`
@@ -33,12 +33,36 @@ func validateCreateUser(r *http.Request) (errs []error) {
 	// Validate User Input
 	validate = validator.New()
 
-	user := UserValidation{
+	createUser := CreateUserValidation{
 		Username: r.FormValue("username"),
 		Email:    r.FormValue("email"),
 		Password: r.FormValue("password"),
 	}
-	vErrs := validate.Struct(&user)
+	vErrs := validate.Struct(&createUser)
+	if vErrs != nil {
+		errs = append(errs, translate.Errors(vErrs, validate)...)
+	}
+
+	// Return nil if no errors
+	return errs
+}
+
+// Login Validation
+type LoginUserValidation struct {
+	Email    string `validate:"required,email"`
+	Password string `validate:"required,min=8"`
+}
+
+func validateLoginUser(r *http.Request) (errs []error) {
+	// Validate User Input
+	validate = validator.New()
+
+	loginUser := LoginUserValidation{
+		Email:    r.FormValue("email"),
+		Password: r.FormValue("password"),
+	}
+
+	vErrs := validate.Struct(&loginUser)
 	if vErrs != nil {
 		errs = append(errs, translate.Errors(vErrs, validate)...)
 	}
