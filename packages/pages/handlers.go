9db16dbf4022b88e5b8fdf7aaf421a4fb200acc5
Larry Chan <1851426+larry-lw-chan@users.noreply.github.com>
2024-03-11 18:17:49 -0400

    Got basic auth working now

--- packages/pages/handlers.go
+++ packages/pages/handlers.go
@@ -3,21 +3,30 @@ package pages
 import (
 	"net/http"
 
+	"github.com/larry-lw-chan/goti/packages/flash"
 	"github.com/larry-lw-chan/goti/packages/utils/render"
 )
 
 func HomePageHandler(w http.ResponseWriter, r *http.Request) {
-	render.Template(w, "pages/home.tmpl", nil)
+	data := make(map[string]interface{})
+	flash := flash.Get(w, r)
+	if flash != nil {
+		data["Flash"] = flash
+	}
+	render.Template(w, "pages/home.tmpl", data)
 }
 
 func AboutPageHandler(w http.ResponseWriter, r *http.Request) {
-	render.Template(w, "pages/about.tmpl", nil)
+	data := make(map[string]interface{})
+	render.Template(w, "pages/about.tmpl", data)
 }
 
 func ContactPageHandler(w http.ResponseWriter, r *http.Request) {
-	render.Template(w, "pages/contact.tmpl", nil)
+	data := make(map[string]interface{})
+	render.Template(w, "pages/contact.tmpl", data)
 }
 
 func PrivacyPageHandler(w http.ResponseWriter, r *http.Request) {
-	render.Template(w, "pages/privacy.tmpl", nil)
+	data := make(map[string]interface{})
+	render.Template(w, "pages/privacy.tmpl", data)
 }
