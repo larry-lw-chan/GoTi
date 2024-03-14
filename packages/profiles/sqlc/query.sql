f7bb16db73887801db07c014972e0268910109fb
Larry Chan <1851426+larry-lw-chan@users.noreply.github.com>
2024-03-13 22:17:12 -0400

    Got profile package up and running

--- packages/profiles/sqlc/query.sql
+++ packages/profiles/sqlc/query.sql
@@ -2,6 +2,6 @@
 SELECT * FROM profiles WHERE user_id = ?;
 
 -- name: CreateProfile :one
-INSERT INTO profiles (name, bio, link, avatar, user_id) 
-VALUES (?, ?, ?, ?, ?)
+INSERT INTO profiles (name, bio, link, avatar, user_id, created_at, updated_at) 
+VALUES (?, ?, ?, ?, ?, ?, ?)
 RETURNING *;
