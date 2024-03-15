47d61c04c978f1faa36011ea6505c9f94183e17f
Larry Chan <1851426+larry-lw-chan@users.noreply.github.com>
2024-03-15 15:50:54 -0400

    Got htmx integrated and working nicely

--- packages/threads/handlers.go
+++ packages/threads/handlers.go
@@ -10,5 +10,13 @@ import (
 * Partials
 *********************************************************/
 func UserThreadsHandler(w http.ResponseWriter, r *http.Request) {
-	render.Partial(w, "/threads/partials/userthreads.app.tmpl", nil)
+	render.Partial(w, "/threads/partials/user_threads.app.tmpl", nil)
+}
+
+func UserRepliesHandler(w http.ResponseWriter, r *http.Request) {
+	render.Partial(w, "/threads/partials/user_replies.app.tmpl", nil)
+}
+
+func UserRepostHandler(w http.ResponseWriter, r *http.Request) {
+	render.Partial(w, "/threads/partials/user_repost.app.tmpl", nil)
 }
