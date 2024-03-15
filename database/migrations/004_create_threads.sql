c41d4bad1f454e6e88dd59e246dd6f581a310279
Larry Chan <1851426+larry-lw-chan@users.noreply.github.com>
2024-03-15 13:16:40 -0400

    Got hashtags schema generated

--- database/migrations/004_create_threads.sql
+++ database/migrations/004_create_threads.sql
@@ -23,10 +23,33 @@ CREATE TABLE likes (
 	FOREIGN KEY("thread_id") 	REFERENCES "threads"("id")
 	FOREIGN KEY("user_id") 		REFERENCES "users"("id")
 );
+
+CREATE TABLE hashtags (
+    "id" 	            INTEGER NOT NULL UNIQUE,
+    "hashtag"		  	TEXT NOT NULL,
+	"created_at"  		TEXT NOT NULL,
+	"updated_at"	    TEXT NOT NULL,
+
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+CREATE TABLE threads_hashtags (
+    "id" 	            INTEGER NOT NULL UNIQUE,
+	"thread_id"			INTEGER NOT NULL,
+	"hashtag_id"		INTEGER NOT NULL,
+	"created_at"  		TEXT NOT NULL,
+	"updated_at"	    TEXT NOT NULL,
+	
+	PRIMARY KEY("id" AUTOINCREMENT)
+	FOREIGN KEY("thread_id") 	REFERENCES "threads"("id")
+	FOREIGN KEY("hashtag_id") 	REFERENCES "hashtags"("id")
+);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
+DROP TABLE threads_hashtags;
+DROP TABLE hashtags;
 DROP TABLE likes;
 DROP TABLE threads;
 -- +goose StatementEnd
--- packages/threads/sqlc/schema.sql
+++ packages/threads/sqlc/schema.sql
@@ -10,6 +10,24 @@ CREATE TABLE threads (
 	FOREIGN KEY("user_id") 		REFERENCES "users"("id")
 );
 
+CREATE TABLE hashtags (
+    "id" 	            INTEGER PRIMARY KEY,
+    "hashtag"		  	TEXT NOT NULL,
+	"created_at"  		TEXT NOT NULL,
+	"updated_at"	    TEXT NOT NULL
+);
+
+CREATE TABLE threads_hashtags (
+    "id" 	            INTEGER PRIMARY KEY,
+	"thread_id"			INTEGER,
+	"hashtag_id"		INTEGER NOT NULL,
+	"created_at"  		TEXT NOT NULL,
+	"updated_at"	    TEXT NOT NULL,
+	
+	FOREIGN KEY("thread_id") 	REFERENCES "threads"("id"),
+	FOREIGN KEY("hashtag_id") 	REFERENCES "hashtags"("id")
+);
+
 CREATE TABLE likes (
     "id" 	            INTEGER PRIMARY KEY,
 	"thread_id"			INTEGER,
