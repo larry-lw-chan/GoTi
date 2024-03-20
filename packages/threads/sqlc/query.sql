57cd6b065c08ae2dfc913a853cbfd1bfa5117ca6
Larry Chan <1851426+larry-lw-chan@users.noreply.github.com>
2024-03-20 06:13:10 -0400

    Added thread abilities

--- packages/threads/sqlc/query.sql
+++ packages/threads/sqlc/query.sql
@@ -2,3 +2,8 @@
 INSERT INTO threads (content, thread_id, user_id, created_at, updated_at)
 VALUES (?, ?, ?, ?, ?)
 RETURNING *;
+
+-- name: GetAllThreads :many
+SELECT content, username
+FROM threads
+JOIN users ON threads.user_id = users.id;
