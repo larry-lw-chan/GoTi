8b9a58fcaf17557fcb97b521d1b2b4898b80b0a7
Larry Chan <1851426+larry-lw-chan@users.noreply.github.com>
2024-03-20 11:43:25 -0400

    Feat: Render Partial now also accepts multiple fiels for partial support

--- packages/utils/render/template.go
+++ packages/utils/render/template.go
@@ -82,12 +82,18 @@ func Partial(w http.ResponseWriter, data map[string]interface{}, files ...string
 
 	// if not available, parse and cache partial
 	if !ok {
-		tmplFile := tmplPath + tmplName
-		tmpl = template.Must(template.ParseFiles(tmplFile))
+		// Compile file list
+		var tmplFiles []string
+		for _, file := range files {
+			tmplFiles = append(tmplFiles, tmplPath+file)
+		}
+
+		tmpl = template.Must(template.ParseFiles(tmplFiles...))
 		tmplCache[tmplName] = tmpl
 	}
+
 	// Execute partial
-	err := tmpl.ExecuteTemplate(w, "partial", data)
+	err := tmpl.Execute(w, data)
 	if err != nil {
 		log.Println(err)
 	}
