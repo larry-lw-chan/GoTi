e2b4dff9fd2254d3eb9998246be44cd4a17d4236
Larry Chan <1851426+larry-lw-chan@users.noreply.github.com>
2024-03-20 05:35:05 -0400

    New thread posting possible now

--- packages/threads/handlers.go
+++ packages/threads/handlers.go
@@ -1,14 +1,19 @@
 package threads
 
 import (
+	"database/sql"
 	"net/http"
+	"time"
 
+	"github.com/larry-lw-chan/goti/database"
 	"github.com/larry-lw-chan/goti/packages/sessions/flash"
+	"github.com/larry-lw-chan/goti/packages/users"
 	"github.com/larry-lw-chan/goti/packages/utils/render"
 )
 
 func NewThreadHandler(w http.ResponseWriter, r *http.Request) {
-	render.Template(w, "/threads/new.app.tmpl", nil)
+	data := r.Context().Value("data").(map[string]any)
+	render.Template(w, "/threads/new.app.tmpl", data)
 }
 
 func NewPostThreadHandler(w http.ResponseWriter, r *http.Request) {
@@ -19,13 +24,35 @@ func NewPostThreadHandler(w http.ResponseWriter, r *http.Request) {
 	if errs != nil {
 		var message string
 		for _, err := range errs {
-			message += err.Error() + "<br />"
+			message += err.Error() + "\n"
 		}
 		flash.Set(w, r, flash.ERROR, message)
 		http.Redirect(w, r, "/threads/new", http.StatusSeeOther)
 		return
 	}
-	render.Template(w, "/threads/new.app.tmpl", nil)
+
+	// Get user session information
+	userSession := users.GetUserSession(r)
+
+	// Create new thread
+	queries := New(database.DB)
+	threadParam := CreateThreadParams{
+		Content:   r.FormValue("content"),
+		ThreadID:  sql.NullInt64{Valid: false},
+		UserID:    userSession.Id,
+		CreatedAt: time.Now().String(),
+		UpdatedAt: time.Now().String(),
+	}
+	_, err := queries.CreateThread(r.Context(), threadParam)
+	if err != nil {
+		flash.Set(w, r, flash.ERROR, "Failed to create new thread.  Please contact support.")
+		http.Redirect(w, r, "/threads/new", http.StatusSeeOther)
+		return
+	}
+
+	// Set flash message and render template
+	flash.Set(w, r, flash.SUCCESS, "New thread created.")
+	http.Redirect(w, r, "/threads/new", http.StatusSeeOther)
 }
 
 /********************************************************
