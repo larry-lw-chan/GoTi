a6fd15bb617c6ac7e733b7f1f5c9bbd0c7f383b5
Larry Chan <1851426+larry-lw-chan@users.noreply.github.com>
2024-03-22 14:37:38 -0400

    Got croppie showing up

--- packages/profiles/handlers.go
+++ packages/profiles/handlers.go
@@ -102,6 +102,21 @@ func EditPostHandler(w http.ResponseWriter, r *http.Request) {
 
 func EditPhotoHandler(w http.ResponseWriter, r *http.Request) {
 	data := r.Context().Value("data").(map[string]interface{})
+
+	// Get user session information
+	userSession := users.GetUserSession(r)
+	queries := New(database.DB)
+
+	// Get profile information
+	profile, err := queries.GetProfileFromUserId(context.Background(), userSession.Id)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// Load username and profile to pass to template
+	data["Username"] = userSession.Username
+	data["Profile"] = profile
+
 	render.Template(w, data, "/profiles/edit-photo.app.tmpl")
 }
 
