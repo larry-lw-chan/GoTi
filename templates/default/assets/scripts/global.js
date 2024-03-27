18dc14cef793a55052d2abba5a1891a5a72a15a0
Larry Chan <1851426+larry-lw-chan@users.noreply.github.com>
2024-03-27 10:58:58 -0400

    Feat: Flash automatically hides in 5 secods

--- templates/default/assets/scripts/global.js
+++
@@ -1,7 +0,0 @@
-window.onload = function () {
-  // Hamburger Menu Toggle
-  document.getElementById("hamburger").addEventListener("click", function () {
-    let header = document.querySelector("header");
-    header.classList.toggle("change");
-  });
-};
